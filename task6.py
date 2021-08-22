e5f2ddc23cb1ce045b94a49516d50718a6e8a6a3
Mumminastya <88561255+Mumminastya@users.noreply.github.com>
2021-08-22 11:32:25 +0300

    Доделано задание 6

--- task6.py
+++ task6.py
@@ -1,21 +1,21 @@
 def run_task6():
-    result = tuple()
+    result = []
     while 1 == 1:
         prod_num = (input("Введите номер товара: "))
         prod_name = (input("Введите название товара: "))
         prod_price = (input("Введите цену товара: "))
-        result = result + (prod_num, {'название': prod_name, 'цена': prod_price})
+        prod_count = (input("Введите количество товара: "))
+        result.append((prod_num, {'название': prod_name, 'цена': prod_price, 'количество': prod_count}))
         quest = input("Хотите ещё? (Д/Н): ")
-        if quest == "Н":
+        if quest.upper() == "Н":
             break
-    print(result)
+    print(f'Товары - {result}')
 
-
-
-
-
-
-
-
-
-
+    analytics = {}
+    for num, properties in result:
+        for prop_key, prop_value in properties.items():
+            if analytics.get(prop_key):
+                analytics[prop_key].append(prop_value)
+            else:
+                analytics[prop_key] = [prop_value]
+    print(f'Аналитика - {analytics}')
